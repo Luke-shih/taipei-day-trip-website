fccd8e712bcc273fa4469162ed6b53785226e6e9
Luke-Shih <s1415937@gmail.com>
2021-04-25 22:05:26 +0800

    add api

--- data/json_file.py
+++ data/json_file.py
@@ -1,6 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
-
 import json, math
 
 db = SQLAlchemy()
@@ -10,7 +9,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:1234@localhost:3306/data"
                                                       # user_name/password/IP/db_name
 i = 0
-def mydefault():
+def default():
     global i
     if i < 12:
         n = 0
@@ -26,19 +25,19 @@ def mydefault():
 
 class travel(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    stitle = db.Column(db.String(255), nullable=False)
+    name = db.Column(db.String(255), nullable=False)
     category = db.Column(db.String(255), nullable=False)
-    transport = db.Column(db.Text)
     description = db.Column(db.Text, nullable=False)
     address = db.Column(db.String(255), nullable=False)
+    transport = db.Column(db.Text)
     MRT = db.Column(db.String(255))
     latitude = db.Column(db.Float, nullable=False)
     longitude = db.Column(db.Float, nullable=False)
     images = db.Column(db.String(255), nullable=False)
-    page = db.Column(db.Integer, primary_key=False, default=mydefault)
+    page = db.Column(db.Integer, primary_key=False, default=default)
 
     def __repr__(self):
-        return f"travel('{self.stitle}', '{self.category}', '{self.description}', '{self.address}', '{self.transport}', '{self.MRT}', '{self.latitude}', '{self.longitude}', '{self.images}', '{self.page}')"
+        return f"travel('{self.name}', '{self.category}', '{self.description}', '{self.address}', '{self.transport}', '{self.MRT}', '{self.latitude}', '{self.longitude}', '{self.images}', '{self.page}')"
 
 def data():
     with open("taipei-attractions.json", mode="r", encoding="utf-8") as file:
@@ -46,21 +45,21 @@ def data():
     finalData = result["result"]["results"]
 
     for result in finalData:
-        stitle = result["stitle"]
+        name = result["stitle"]
         category = result["CAT2"]
-        transport = result['info']
         description = result['xbody']
         address = result['address']
+        transport = result['info']
         MRT = result['MRT']
         latitude = result['latitude']
         longitude = result["longitude"]
         images = result["file"].replace("http", " http").split(' ')[1]
 
-        trip = travel(stitle=stitle, category=category, transport=transport, description=description, address=address, MRT=MRT, latitude=latitude, longitude=longitude, images=images)
+        trip = travel(name=name, category=category, description=description, address=address, transport=transport, MRT=MRT, latitude=latitude, longitude=longitude, images=images)
         db.session.add(trip)
         db.session.commit()
 
-if __name__ == '__main__':
+if __name__ == '__main__':  # 程序的入口
     with app.app_context():
         db.init_app(app)
         db.create_all()
