4a7d3f25425ebb49f83933892e4d10fa21bfa662
Luke-Shih <s1415937@gmail.com>
2021-04-26 15:34:20 +0800

    change the starting number of the page to zero

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app=Flask(__name__)
 app.config["JSON_AS_ASCII"]=False # False 避免中文顯示為ASCII編碼
 app.config["TEMPLATES_AUTO_RELOAD"]=True # True 當 flask 偵測到 template 有修改會自動更新
 app.config["JSON_SORT_KEYS"]=False # False 不以物件名稱進行排序顯示
-app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:@localhost:3306/data"
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:1234@localhost:3306/data"
 
 
 @app.route("/")
@@ -66,7 +66,6 @@ travelSchema = travelSchema(many=True)
 @app.route("/api/attractions")
 def attractions():
     page = request.args.get('page', 0, type = int)
-    page = page-1
     keyword = request.args.get('keyword', None)
 
     if keyword is None:
